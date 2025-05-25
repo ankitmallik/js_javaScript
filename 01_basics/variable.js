e9072fa3d04e8a2afb729813094adbb8cfb53348
ankitmallik <138785038+ankitmallik@users.noreply.github.com>
2025-05-25 22:00:04 +0000

    practice variable and scope

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const number = 8862900
+let name = "ankit mallik"
+let email = "ankit@gmail.com"
+var city = "darbhanga"
+var country = "india"
+
+// number = 88888
+name = "john"
+email = "mallik@gmaail.com"
+city = "us"
+country = "new York"
+console.table([number,name,email,city,country])
